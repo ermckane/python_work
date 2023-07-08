38a7842d24baa544085d6a48c3434ead9660a506
unknown <ermckane@gmail.com>
2023-07-08 10:59:26 -0400

    Working through exercises

---
+++ crash_course/chapter_2/exercises/exercise_2_10.py
@@ -0,0 +1,4 @@
+count = 0
+for x in range(10):
+    print(count + 1)
+    count += 1
---
+++ crash_course/chapter_2/exercises/exercise_2_9.py
@@ -0,0 +1,7 @@
+spam = str(input())
+if spam == '1':
+    print('Hello')
+elif spam == '2':
+    print('Howdy')
+else:
+    print('Greetings')
