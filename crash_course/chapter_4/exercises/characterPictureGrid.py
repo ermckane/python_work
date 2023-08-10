d3092b5b3922980c1fd75e9de99a1fbeddfecfcc
ermckane <ermckane@gmail.com>
2023-08-10 08:26:16 -0400

    Error preventing from preventing from printing after 2 lines. Need to resolve.

---
+++ crash_course/chapter_4/exercises/characterPictureGrid.py
@@ -0,0 +1,20 @@
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+
+xCounter = len(grid) - 1
+yCounter = 0
+
+while True:
+        for y in grid:
+                print(grid[xCounter][yCounter], end = ' ')
+                xCounter -= 1
+        print()
+        yCounter +=1
